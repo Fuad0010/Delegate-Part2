97faa7e3f259d79d47b7b07c42d321acee9e833d
Fuad0010 <fuadft@code.edu.az>
2022-04-06 17:43:22 +0400

    Add project files.

---
+++ Delegate-Part2/Models/IEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegate_Part2.Models
+{
+    internal interface IEntity
+    {
+        private static int _id;
+        public int Id{ get { return _id; } }
+        void IEntity()
+        {
+            _id++;
+            
+        }
+        
+    }
+}
